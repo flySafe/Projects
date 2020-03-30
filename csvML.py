cfc2516ffef465890b712c3e1f5f3e3df25287e8
ArielBlG <62558598+ArielBlG@users.noreply.github.com>
2020-03-30 11:03:47 +0300

    Add files via upload

---
+++ csvML.py
@@ -0,0 +1,93 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import display
+import xgboost as xgb
+from xgboost import plot_importance, plot_tree
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn import preprocessing
+
+test = pd.read_csv("/Users/ariel-pc/Downloads/test.csv")
+train = pd.read_csv("/Users/ariel-pc/Downloads/train.csv")
+
+# confirmed_total_date = train.groupby(['Date']).agg({'ConfirmedCases':['sum']})
+# fatalities_total_date = train.groupby(['Date']).agg({'Fatalities':['sum']})
+# total_date = confirmed_total_date.join(fatalities_total_date)
+#
+# fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17,7))
+# total_date.plot(ax=ax1)
+# ax1.set_title("Global confirmed cases", size=13)
+# ax1.set_ylabel("Number of cases", size=13)
+# ax1.set_xlabel("Date", size=13)
+# fatalities_total_date.plot(ax=ax2, color='orange')
+# ax2.set_title("Global deceased cases", size=13)
+# ax2.set_ylabel("Number of cases", size=13)
+# ax2.set_xlabel("Date", size=13)
+# plt.show()
+# Merge train and test, exclude overlap
+dates_overlap = ['2020-03-19','2020-03-20','2020-03-21','2020-03-22','2020-03-23', '2020-03-24', '2020-03-25', '2020-03-26', '2020-03-27']
+train2 = train.loc[~train['Date'].isin(dates_overlap)]
+all_data = pd.concat([train2, test], axis = 0, sort=False)
+
+# Double check that there are no informed ConfirmedCases and Fatalities after 2020-03-11
+all_data.loc[all_data['Date'] >= '2020-03-19', 'ConfirmedCases'] = np.nan
+all_data.loc[all_data['Date'] >= '2020-03-19', 'Fatalities'] = np.nan
+all_data['Date'] = pd.to_datetime(all_data['Date'])
+
+# Create date columns
+le = preprocessing.LabelEncoder()
+all_data['Day_num'] = le.fit_transform(all_data.Date)
+all_data['Day'] = all_data['Date'].dt.day
+all_data['Month'] = all_data['Date'].dt.month
+all_data['Year'] = all_data['Date'].dt.year
+
+# Fill null values given that we merged train-test datasets
+all_data['Province_State'].fillna("None", inplace=True)
+all_data['ConfirmedCases'].fillna(0, inplace=True)
+all_data['Fatalities'].fillna(0, inplace=True)
+all_data['Id'].fillna(-1, inplace=True)
+all_data['ForecastId'].fillna(-1, inplace=True)
+
+# display(all_data)
+# display(all_data.loc[all_data['Date'] == '2020-03-19'])
+
+missings_count = {col:all_data[col].isnull().sum() for col in all_data.columns}
+missings = pd.DataFrame.from_dict(missings_count, orient='index')
+# print(missings.nlargest(30, 0))
+
+world_population = pd.read_csv("/Users/ariel-pc/Downloads/population_by_country_2020.csv")
+
+# Select desired columns and rename some of them
+world_population = world_population[['Country (or dependency)', 'Population (2020)', 'Density (P/Km²)', 'Land Area (Km²)', 'Med. Age', 'Urban Pop %']]
+world_population.columns = ['Country (or dependency)', 'Population (2020)', 'Density', 'Land Area', 'Med Age', 'Urban Pop']
+
+# Replace United States by US
+world_population.loc[world_population['Country (or dependency)']=='United States', 'Country (or dependency)'] = 'US'
+
+# Remove the % character from Urban Pop values
+world_population['Urban Pop'] = world_population['Urban Pop'].str.rstrip('%')
+
+# Replace Urban Pop and Med Age "N.A" by their respective modes, then transform to int
+world_population.loc[world_population['Urban Pop']=='N.A.', 'Urban Pop'] = int(world_population.loc[world_population['Urban Pop']!='N.A.', 'Urban Pop'].mode()[0])
+world_population['Urban Pop'] = world_population['Urban Pop'].astype('int16')
+world_population.loc[world_population['Med Age']=='N.A.', 'Med Age'] = int(world_population.loc[world_population['Med Age']!='N.A.', 'Med Age'].mode()[0])
+world_population['Med Age'] = world_population['Med Age'].astype('int16')
+
+# Now join the dataset to our previous DataFrame and clean missings (not match in left join)- label encode cities
+#print("Joined dataset")
+all_data = all_data.merge(world_population, left_on='Country_Region', right_on='Country (or dependency)', how='left')
+all_data[['Population (2020)', 'Density', 'Land Area', 'Med Age', 'Urban Pop']] = all_data[['Population (2020)', 'Density', 'Land Area', 'Med Age', 'Urban Pop']].fillna(0)
+
+# Label encode countries and provinces. Save dictionary for exploration purposes
+all_data.drop('Country (or dependency)', inplace=True, axis=1)
+all_data['Country_Region'] = le.fit_transform(all_data['Country_Region'])
+number_c = all_data['Country_Region']
+countries = le.inverse_transform(all_data['Country_Region'])
+country_dict = dict(zip(countries, number_c))
+all_data['Province_State'] = le.fit_transform(all_data['Province_State'])
+number_p = all_data['Province_State']
+province = le.inverse_transform(all_data['Province_State'])
+province_dict = dict(zip(province, number_p))
+print(country_dict)
